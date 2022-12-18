652fc4c94fa782f28e143b3a683201828d2bd30a
AnveshakR <rathoreanveshak@gmail.com>
2022-12-17 21:06:46 -0500

    added port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn webapp:app
+web: flask webapp:app
--- webapp.py
+++ webapp.py
@@ -36,4 +36,4 @@ def result():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))

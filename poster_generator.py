0d8ae336c67f072b2121ece8188c902deba7680e
AnveshakR <rathoreanveshak@gmail.com>
2022-12-24 14:00:24 -0500

    added GPL v3

--- poster_generator.py
+++ poster_generator.py
@@ -1,3 +1,13 @@
+"""
+     This file is part of poster-gen.
+
+    poster-gen is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    poster-gen is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with poster-gen. If not, see <https://www.gnu.org/licenses/>. 
+"""
+
 import numpy as np
 from PIL import Image
 import cv2
--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
+"""
+     This file is part of poster-gen.
+
+    poster-gen is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+    poster-gen is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with poster-gen. If not, see <https://www.gnu.org/licenses/>. 
+"""
+
 import cv2
 import numpy as np
 import scipy.cluster
